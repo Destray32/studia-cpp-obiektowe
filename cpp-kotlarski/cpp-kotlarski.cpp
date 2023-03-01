1b13387cf0f9929d1cb896383b0accba9d2bf7fb
Destray32 <kubaj38@gmail.com>
2023-03-01 22:54:27 +0100

    init, laby drugie

---
+++ cpp-kotlarski/cpp-kotlarski.cpp
@@ -0,0 +1,478 @@
+﻿#include <iostream>
+#include <string>
+#include <algorithm>
+#include <vector>
+#include <fstream>
+#include <string>
+
+void zadanieJeden()
+{
+	std::string liczby = "";
+	int suma = 0;
+
+	std::cin >> liczby;
+
+	for (int i = 0; i < liczby.length(); i += 2)
+	{
+		std::cout << liczby[i] << '\n';
+		suma += liczby[i] - '0';
+
+		if (liczby[i] == '0')
+			break;
+	}
+	std::cout << "Suma = " << suma << '\n';
+}
+
+void zadanieDrugie()
+{
+	bool czyPitagoras = false;
+	int* p_podstawa = new int;
+	int* p_bokJeden = new int;
+	int* p_bokDwa = new int;
+
+	std::cin >> *p_podstawa;
+	std::cin >> *p_bokJeden;
+	std::cin >> *p_bokDwa;
+
+	std::cout << *p_podstawa << ' ' << *p_bokJeden << ' ' << *p_bokDwa << '\n';
+
+	int kwadratPodst = (*p_podstawa) * (*p_podstawa);
+	int kwadratBokJed = (*p_bokJeden) * (*p_bokJeden);
+	int kwadratBokDwa = (*p_bokDwa) * (*p_bokDwa);
+
+	//std::cout << "\ntest\n";
+	//std::cout << kwadratPodst << ' ' << kwadratBokJed << ' ' << kwadratBokDwa;
+
+
+	if (kwadratPodst + kwadratBokJed == kwadratBokDwa)
+		std::cout << "Trojkat jest pitagorejski\n";
+	else
+		std::cout << "Trojkat nie jest pitagorejski\n";
+
+	if (*p_podstawa == *p_bokJeden && *p_podstawa == *p_bokDwa)
+		std::cout << "Trojkat jest rownoboczny\n";
+	else
+		std::cout << "Trojkat nie jest rownoboczny\n";
+
+	if (*p_bokJeden == *p_bokDwa)
+		std::cout << "Trojkat jest rownoramienny\n";
+	else
+		std::cout << "Trojkat nie jest rownoramienny\n";
+
+	delete p_podstawa;
+	delete p_bokJeden;
+	delete p_bokDwa;
+}
+
+void zadanieTrzecie()
+{
+	for (int i = 1; i < 11; i++)
+	{
+		for (int j = 1; j < 11; j++)
+		{
+			std::cout << j * i << ',';
+		}
+		std::cout << '\n';
+	}
+}
+
+void zadanieCzwarte()
+{
+	int cyfra = 0;
+
+	std::cout << "Podaj cyfre :\n";
+	std::cin >> cyfra;
+	bool stan = false;
+
+	for (int i = 0; i < cyfra; i++)
+	{
+		for (int j = 0; j < cyfra; j++)
+		{
+			if (stan == false)
+				if (j % 2 != 0)
+					std::cout << "1" << ',';
+				else
+					std::cout << "0" << ',';
+			else
+				if (j % 2 != 0)
+					std::cout << "0" << ',';
+				else
+					std::cout << "1" << ',';
+		}
+		stan = !stan;
+		std::cout << '\n';
+	}
+}
+
+void zadaniePiate()
+{
+	std::vector<int> tablica(20);
+
+	std::cout << "Podaj 20 cyfr, ktore maja zostac zapisane do tablicy\n";
+
+	for (int i = 0; i < tablica.size(); i++)
+	{
+		int liczba = 0;
+		std::cin >> liczba;
+		tablica.at(i) = liczba;
+	}
+
+	std::cout << "\n\n";
+	std::sort(tablica.begin(), tablica.end());
+
+
+	std::cout << "Najwieksza cyfra: " << tablica[tablica.size() - 1]
+		<< '\n' << "Najmniejsza cyfra: " << tablica[0] << '\n' << "Suma cyfr liczb parzystych :" <<
+		[&]() -> int { int sum = 0; for (auto v : tablica) (v % 2 == 0) ? sum += v : sum += 0; return sum; }();
+
+	std::cin.get();
+}
+
+void zadanieSzoste()
+{
+	int tabA[4][4] = {};
+	int tabB[4][4] = {};
+
+	for (int i = 0; i < 4; i++)
+	{
+		for (int j = 0; j < 4; j++)
+		{
+			switch (i)
+			{
+			case 0:
+				tabA[i][j] = 2;
+				break;
+			case 1:
+				tabA[i][j] = 4;
+				break;
+			case 2:
+				tabA[i][j] = 6;
+				break;
+			case 3:
+				tabA[i][j] = 8;
+				break;
+			}
+		}
+	}
+
+	for (int i = 0; i < 4; i++)
+	{
+		for (int j = 0; j < 4; j++)
+		{
+			tabB[i][j] = tabA[j][i];
+		}
+	}
+
+	for (int i = 0; i < 4; i++)
+	{
+		for (int j = 0; j < 4; j++)
+		{
+			std::cout << tabA[i][j];
+			std::cout << ' ';
+		}
+		std::cout << '\n';
+	}
+
+	std::cout << '\n';
+	std::cout << '\n';
+
+	for (int i = 0; i < 4; i++)
+	{
+		for (int j = 0; j < 4; j++)
+		{
+			std::cout << tabB[i][j];
+			std::cout << ' ';
+		}
+		std::cout << '\n';
+	}
+
+}
+
+void zadanieSiodme()
+{
+	int tabA[4][4] = {};
+	int tabB[4][4] = {};
+	int tabC[4][4] = {};
+
+	int counter = 0;
+	bool stan = false;
+
+	for (int i = 0; i < 4; i++)
+	{
+		counter = 0;
+		for (int j = 0; j < 4; j++)
+		{
+			tabA[i][j] = 1;
+			counter++;
+
+			if (counter == (i + 1))
+				break;
+		}
+	}
+
+	for (int i = 0; i < 4; i++)
+	{
+		for (int j = 0; j < 4; j++)
+		{
+			if (stan)
+			{
+				if (j % 2 == 0)
+					tabB[i][j] = 1;
+			}
+			else
+				if (j % 2 == 1)
+					tabB[i][j] = 1;
+		}
+		stan = !stan;
+	}
+
+	for (int i = 0; i < 4; i++)
+	{
+		counter = 0;
+		for (int j = 0; j < 4; j++)
+		{
+			tabC[i][j] = (tabA[i][j] + tabB[i][j]);
+		}
+	}
+
+	for (int i = 0; i < 4; i++)
+	{
+		for (int j = 0; j < 4; j++)
+		{
+			std::cout << tabA[i][j];
+			std::cout << ' ';
+		}
+		std::cout << '\n';
+	}
+
+	std::cout << '\n';
+	std::cout << '\n';
+
+	for (int i = 0; i < 4; i++)
+	{
+		for (int j = 0; j < 4; j++)
+		{
+			std::cout << tabB[i][j];
+			std::cout << ' ';
+		}
+		std::cout << '\n';
+	}
+
+	std::cout << '\n';
+	std::cout << '\n';
+
+	for (int i = 0; i < 4; i++)
+	{
+		for (int j = 0; j < 4; j++)
+		{
+			std::cout << tabC[i][j];
+			std::cout << ' ';
+		}
+		std::cout << '\n';
+	}
+
+	std::cout << '\n';
+	std::cout << '\n';
+
+	std::cin.get();
+}
+
+void zadanieOsme()
+{
+	std::ifstream file("dane.txt");
+
+	std::vector<int> liczby(100);
+	int counter = 0;
+
+	std::string line;
+	while (std::getline(file, line))
+	{
+		int liczba = std::stoi(line);
+		liczby.at(liczba) += 1;
+	}
+
+	for (auto v : liczby)
+	{
+		std::cout << '\'' << counter << '\'' << "->" << v << '\n';
+		counter++;
+	}
+
+	std::cin.get();
+}
+
+void zadanieDziewiec()
+{
+	std::ofstream plik1("zad1.txt");
+	std::ofstream plik2("zad2.txt");
+	std::ofstream plik3("wynik.txt");
+	int counter = 0;
+
+	int tabA[4][4] = {};
+	int tabB[4][4] = {};
+	int tabSuma[4][4] = {};
+
+
+	for (int i = 0; i < 4; i++)
+	{
+		counter = 0;
+		for (int j = 0; j < 4; j++)
+		{
+			tabA[i][j] = 1;
+			counter++;
+
+			if (counter == (i + 1))
+				break;
+		}
+	}
+
+	for (int i = 0; i < 4; i++)
+	{
+		counter = 0;
+		for (int j = 0; j < 4; j++)
+		{
+			if (i == 1)
+				tabB[i][j] = 3;
+			if (i == 3)
+				tabB[i][j] = 3;
+		}
+	}
+
+	for (int i = 0; i < 4; i++)
+	{
+		counter = 0;
+		for (int j = 0; j < 4; j++)
+		{
+			tabSuma[i][j] = (tabA[i][j] + tabB[i][j]);
+		}
+	}
+
+	for (int i = 0; i < 4; i++)
+	{
+		for (int j = 0; j < 4; j++)
+		{
+			plik1 << tabA[i][j];
+			plik1 << ' ';
+		}
+		plik1 << '\n';
+	}
+
+	for (int i = 0; i < 4; i++)
+	{
+		for (int j = 0; j < 4; j++)
+		{
+			plik2 << tabB[i][j];
+			plik2 << ' ';
+		}
+		plik2 << '\n';
+	}
+
+	for (int i = 0; i < 4; i++)
+	{
+		for (int j = 0; j < 4; j++)
+		{
+			plik3 << tabSuma[i][j];
+			plik3 << ' ';
+		}
+		plik3 << '\n';
+	}
+
+	std::cin.get();
+}
+
+int mnozenie(int& x, int& y)
+{
+	// należy użyć, deklarując dwie zmienne w main. 
+	return x * y;
+}
+
+int max_tab(int t[], int n)
+{
+	int max = t[0];
+	for (int i = 1; i < n; i++)
+	{
+		if (t[i] > max)
+			max = t[i];
+	}
+	return max;
+
+	// ciało main poniżej:
+
+	/*int tablica[] = { 2, 4, 10, 1, 1, 2, 4, 5, 1, 3 };
+	int n = sizeof(tablica) / sizeof(tablica[0]);
+
+	std::cout << max_tab(tablica, n);*/
+}
+
+int* silnia(int* a)
+{
+	return 0;
+}
+
+typedef struct {
+	int re; //część rzeczywista
+	int im; //część urojona
+}zespolona;
+
+void zadanieTrzynaste()
+{
+	zespolona zs1;
+	zespolona zs2;
+	
+	std::cout << "Wpisz czesc rzeczywista i urojona pierwszej liczby zespolonej: ";
+	std::cin >> zs1.re;
+	std::cin >> zs1.im;
+	
+	std::cout << "Wpisz czesc rzeczywista i urojona drugiej liczby zespolonej: ";
+	std::cin >> zs2.re;
+	std::cin >> zs2.im;
+	
+	std::cout << "Suma liczb zespolonych wynosi: " << zs1.re + zs2.re 
+		<< " + " << zs1.im + zs2.im << "i" << '\n';
+
+	std::cin.get();
+}
+
+typedef struct {
+	int wysokosc;
+	int szerokosc;
+}prostokat;
+
+void zadanieCzternaste()
+{
+	prostokat* p_p1 = new prostokat;
+	int wys = 0;
+	int szer = 0;
+	int pole = 0;
+	int obwod = 0;
+	
+	std::cout << "Podaj wysokosc prostokata: ";
+	std::cin >> wys;
+	std::cout << "Podaj szerokosc prostokata: ";
+	std::cin >> szer;
+
+	p_p1->wysokosc = wys;
+	p_p1->szerokosc = szer;
+
+	pole = p_p1->szerokosc * p_p1->wysokosc;
+	obwod = (p_p1->szerokosc * 2) + (p_p1->wysokosc * 2);
+
+	for (int i = 0; i < wys; i++)
+	{
+		for (int i = 0; i < szer; i++)
+		{
+			std::cout << '*';
+		}
+		std::cout << '\n';
+	}
+
+	std::cout << "\n\n Pole: " << pole << "\n Obwod: " << obwod << "\n";
+	
+	std::cin.get();
+	delete p_p1;
+}
+
+int main()
+{
+	
+}
+
---
+++ cpp-kotlarski/zaj2.cpp
@@ -0,0 +1,439 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+
+// Zadanie 1
+///
+#pragma region Zadanie 1
+
+class kalkulator {
+private:
+	int liczba1;
+	int liczba2;
+public:
+	kalkulator(void);
+	~kalkulator(void);
+	int dodawanie(void);
+	int odejmowanie(void);
+	int mnozenie(void);
+	void klawiatura(void);
+};
+
+kalkulator::kalkulator(void)
+{
+	this->liczba1 = 0;
+	this->liczba2 = 0;
+}
+
+kalkulator::~kalkulator(void)
+{
+
+}
+
+int kalkulator::dodawanie(void)
+{
+	return (this->liczba1 + liczba2);
+}
+
+int kalkulator::odejmowanie(void)
+{
+	return (this->liczba1 - this->liczba2);
+}
+
+int kalkulator::mnozenie(void)
+{
+	return (this->liczba1 * this->liczba2);
+}
+
+void kalkulator::klawiatura(void)
+{
+	int l1 = 0;
+	int l2 = 0;
+	std::cout << "Podaj pierwsza liczbe : ";
+	std::cin >> l1;
+	std::cout << "\nPodaj druga liczbe : ";
+	std::cin >> l2;
+
+	this->liczba1 = l1;
+	this->liczba2 = l2;
+}
+
+//int main()
+//{
+//	kalkulator kalk;
+//	kalk.klawiatura();
+//	std::cout << "Wynik dodawania : " << kalk.dodawanie() << std::endl;
+//	std::cout << "Wynik odejmowania : " << kalk.odejmowanie() << std::endl;
+//	std::cout << "Wynik mnozenia : " << kalk.mnozenie() << std::endl;
+//	
+//	std::cin.get();
+//}
+#pragma endregion
+
+// Zadanie 2
+///
+
+#pragma region Zadanie 2
+
+class operacje {
+private:
+	int liczba1;
+	int liczba2;
+public:
+	operacje(void);
+	~operacje(void);
+	int wieksza(void);
+	int mniejsza(void);
+	int czy_rowne(void);
+	void klawiatura(void);
+};
+
+operacje::operacje(void)
+{
+	this->liczba1 = 0;
+	this->liczba2 = 0;
+}
+
+operacje::~operacje(void)
+{
+
+}
+
+int operacje::wieksza(void)
+{
+	return (this->liczba1 > this->liczba2) ? this->liczba1 : this->liczba2;
+}
+
+int operacje::mniejsza(void)
+{
+	return (this->liczba1 < liczba2) ? this->liczba1 : this->liczba2;
+}
+
+int operacje::czy_rowne(void)
+{
+	return (this->liczba1 == liczba2) ? 1 : 0;
+}
+
+void operacje::klawiatura(void)
+{
+	int l1 = 0;
+	int l2 = 0;
+	std::cout << "Podaj pierwsza liczbe : ";
+	std::cin >> l1;
+	std::cout << "\nPodaj druga liczbe : ";
+	std::cin >> l2;
+
+	this->liczba1 = l1;
+	this->liczba2 = l2;
+}
+
+//int main()
+//{
+//	operacje oper;
+//	oper.klawiatura();
+//	std::cout << "Wieksza liczba : " << oper.wieksza() << std::endl;
+//	std::cout << "Mniejsza liczba : " << oper.mniejsza() << std::endl;
+//	
+//	if (oper.czy_rowne() == 1)
+//		std::cout << "Liczby sa rowne" << std::endl;
+//	else
+//		std::cout << "Liczby nie sa rowne" << std::endl;
+//
+//	std::cin.get();
+//}
+
+#pragma endregion
+
+// Zadanie 3
+///
+
+#pragma region Zadanie 3
+
+class algorytmy {
+private:
+	int liczba1;
+	int liczba2;
+	int liczba3;
+	int liczba4;
+public:
+	algorytmy(void);
+	~algorytmy(void);
+	int max(void);
+	int mini(void);
+	int suma(void);
+	int szukana(int x);
+	int parzyste(void);
+	void klawiatura(void);
+};
+
+algorytmy::algorytmy(void)
+{
+	this->liczba1 = 0;
+	this->liczba2 = 0;
+	this->liczba3 = 0;
+	this->liczba4 = 0;
+}
+
+algorytmy::~algorytmy(void)
+{
+}
+
+int algorytmy::max(void)
+{
+	std::vector<int> liczby = { liczba1, liczba2, liczba3, liczba4 };
+	return *std::max_element(liczby.begin(), liczby.end());
+
+}
+
+int algorytmy::mini(void)
+{
+	std::vector<int> liczby = { liczba1, liczba2, liczba3, liczba4 };
+	return *std::min_element(liczby.begin(), liczby.end());
+}
+
+int algorytmy::suma(void)
+{
+	int suma = 0;
+	std::vector<int> liczby = { liczba1, liczba2, liczba3, liczba4 };
+
+	for (auto l : liczby)
+	{
+		suma += l;
+	}
+	return suma;
+}
+
+int algorytmy::szukana(int x)
+{
+	std::vector<int> liczby = { liczba1, liczba2, liczba3, liczba4 };
+	auto it = std::find(liczby.begin(), liczby.end(), x);
+
+	if (it != liczby.end())
+		return 1;
+	else
+		return 0;
+}
+
+int algorytmy::parzyste(void)
+{
+	int parzyste = 0;
+	std::vector<int> liczby = { liczba1, liczba2, liczba3, liczba4 };
+
+	for (auto l : liczby)
+	{
+		if (l % 2 == 0)
+			parzyste++;
+	}
+
+	return parzyste != 0 ? 1 : 0;
+}
+
+void algorytmy::klawiatura(void)
+{
+	std::cout << "Podaj cztery liczby : ";
+	std::cin >> liczba1 >> liczba2 >> liczba3 >> liczba4;
+}
+
+//int main()
+//{
+//	algorytmy alg;
+//	alg.klawiatura();
+//	std::cout << "Maksymalna liczba : " << alg.max() << std::endl;
+//	std::cout << "Minimalna liczba : " << alg.mini() << std::endl;
+//	std::cout << "Suma liczb : " << alg.suma() << std::endl;
+//
+//	if (alg.szukana(5))
+//		std::cout << "Liczba 5 znajduje sie w tablicy" << std::endl;
+//	else
+//		std::cout << "Liczba 5 nie znajduje sie w tablicy" << std::endl;
+//
+//	if (alg.parzyste())
+//		std::cout << "W tablicy znajduja sie liczby parzyste" << std::endl;
+//	else
+//		std::cout << "W tablicy nie znajduja sie liczby parzyste" << std::endl;
+//}
+
+#pragma endregion
+
+// Zadanie 4
+///
+
+#pragma region Zadanie 4
+
+class trojkat {
+private:
+	int podstawa;
+	int bok1;
+	int bok2;
+	int wysokosc;
+public:
+	trojkat(void);
+	~trojkat(void);
+	int obwod(void);
+	double pole(void);
+	int pitagoras(void);
+	int czy_rownoboczny(void);
+	int czy_rownoramienny(void);
+	void klawiatura(void);
+};
+
+
+trojkat::trojkat(void)
+{
+	this->podstawa = 0;
+	this->bok1 = 0;
+	this->bok2 = 0;
+	this->wysokosc = 0;
+}
+
+trojkat::~trojkat(void)
+{
+}
+
+int trojkat::obwod(void)
+{
+	return (this->podstawa + this->bok1 + this->bok2);
+}
+
+double trojkat::pole(void)
+{
+	return (this->podstawa * this->wysokosc) / 2;
+}
+
+int trojkat::pitagoras(void)
+{
+	return ((this->bok1 * this->bok1) + (this->bok2 * this->bok2) == (this->podstawa * this->podstawa)) ? 1 : 0;
+}
+
+int trojkat::czy_rownoboczny(void)
+{
+	return (this->bok1 + this->bok2 == this->podstawa) ? 1 : 0;
+}
+
+int trojkat::czy_rownoramienny(void)
+{
+	return (this->bok1 == bok2) ? 1 : 0;
+}
+
+void trojkat::klawiatura(void)
+{
+	std::cout << "Podaj kolejno podstawe, bok pierwszy, bok drugi i wysokosc : ";
+	std::cin >> podstawa >> bok1 >> bok2 >> wysokosc;
+}
+
+//int main()
+//{
+//	trojkat trk;
+//	trk.klawiatura();
+//	std::cout << "Obwod trojkata: " << trk.obwod() << std::endl;
+//	std::cout << "Pole trojkata: " << trk.pole() << std::endl;
+//	
+//	if (trk.pitagoras())
+//		std::cout << "Trojkat jest prostokatny" << std::endl;
+//	else
+//		std::cout << "Trojkat nie jest prostokatny" << std::endl;
+//
+//	if (trk.czy_rownoboczny())
+//		std::cout << "Trojkat jest rownoboczny" << std::endl;
+//	else
+//		std::cout << "Trojkat nie jest rownoboczny" << std::endl;
+//
+//	if (trk.czy_rownoramienny())
+//		std::cout << "Trojkat jest rownoramienny" << std::endl;
+//	else
+//		std::cout << "Trojkat nie jest rownoramienny" << std::endl;
+//
+//}
+
+#pragma endregion
+
+// Zadanie 5
+///
+
+#pragma region Zadanie 5
+
+class prostokat {
+private:
+	int bok1;
+	int bok2;
+public:
+	prostokat(void);
+	~prostokat(void);
+	int obwod(void);
+	int pole(void);
+	int czy_kwadrat(void);
+	void klawiatura(void);
+};
+
+
+
+prostokat::prostokat(void)
+{
+}
+
+prostokat::~prostokat(void)
+{
+	this->bok1 = 0;
+	this->bok2 = 0;
+}
+
+int prostokat::obwod(void)
+{
+	return (2 * this->bok1) + (2 * this->bok2);
+}
+
+int prostokat::pole(void)
+{
+	return (this->bok1 * this->bok2);
+}
+
+int prostokat::czy_kwadrat(void)
+{
+	return (this->bok1 == this->bok2) ? 1 : 0;
+}
+
+void prostokat::klawiatura(void)
+{
+	std::cout << "Podaj kolejno dlugosc pierwszego boku, dlugosc drugiego boku : ";
+	std::cin >> bok1 >> bok2;
+}
+
+//int main()
+//{
+//	prostokat prost;
+//	prost.klawiatura();
+//	
+//	std::cout << "Obwod prostokata: " << prost.obwod() << std::endl;
+//	std::cout << "Pole prostokata: " << prost.pole() << std::endl;
+//	
+//	if (prost.czy_kwadrat())
+//		std::cout << "Prostokat jest kwadratem" << std::endl;
+//	else
+//		std::cout << "Prostokat nie jest kwadratem" << std::endl;
+//}
+
+#pragma endregion
+
+// Zadanie 6
+///
+
+#pragma region Zadanie 6
+typedef struct
+{
+	double x1;
+	double x2;
+}wynik;
+class rownanie {
+private:
+	int a;
+	int b;
+	int c;
+public:
+	rownanie(void);
+	~rownanie(void);
+	void klawiatura(void);
+	int delta(void);
+	double jeden(void);
+	wynik dwa(void);
+};
+#pragma endregion
